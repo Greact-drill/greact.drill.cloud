8bbbe261ce9807dc499c511931ed5db1bd263959
Первов Артем <apervov@alabuga.ru>
2025-10-06 03:28:08 +0300

    added cors in main.ts for admin service

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // app.enableCors({
-  //   origin: 'http://localhost:5173', 
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   allowedHeaders: 'Content-Type, Accept, Authorization',
-  //   credentials: true,
-  // });
+  app.enableCors({
+    origin: 'https://admin.drill.greact.ru', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
+  });
 
   // Включаем глобальную валидацию
   app.useGlobalPipes(new ValidationPipe({
